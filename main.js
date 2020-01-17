ac6334994212a9a8a881671897495054e06287a4
JihyeLee_SOPHIA <sophapia@users.noreply.github.com>
2020-01-17 19:55:55 +0900

    Update jump function
    FIx an error with jump and move actions not to stop the character after press 'space-bar'

--- main.js
+++ main.js
@@ -14,7 +14,13 @@ tree.src = 'images/tree.png';
 manr1.src = 'images/manr1.png';
 manr2.src = 'images/manr2.png';
 spike1.src = 'images/spike monster B.png';
-fruit.src = 'images/fruit3.png';
+fruit.src = 'images/fruit3.png'
+
+//audio
+const eat = new Audio();
+const gamestart = new Audio();
+eat.src = 'audio/score.mp3'
+gamestart.src = 'audio/music.wav';
 
 const trees = [];
 trees[0] = {
@@ -34,10 +40,11 @@ fruits[0] = {
 let score = 0;
 let charM = true;
 let charC = 0;
+let charX = 80;
 let charY = 430;
-let keyPressed = {};
 let moveBG1 = '';
 let moveBG2 = '';
+let rightEvent = false;
 
 function moveTree() {
     for (let i = 0; i < trees.length; i++) {
@@ -61,9 +68,12 @@ function moveCharater() {
     } else {
         charM = true;
     }
+    if (charC % 30 === 0) {
+        score++;
+    }
 }
 
-function movemove(){
+function movemove() {
     moveBG1 = setTimeout(function () {
         moveTree();
         moveSpikes();
@@ -72,50 +82,36 @@ function movemove(){
     }, 30)
 }
 function keydownHandler(event) {
-    // event.preventDefault();
-    keyPressed[event.key] = true;
-    if (keyPressed['ArrowRight']) {
-        if (charC % 30 === 0) {
-            score++;
-        }
-        movemove();
+    if (event.key === 'ArrowRight') {
+        rightEvent = true;
     }
-    if (keyPressed[' ']&&keyPressed['ArrowRight']) {
-        if (charC % 30 === 0) {
-            score++;
-        }
-            moveBG2 = setInterval(function(){
-            moveTree();
-            moveSpikes();
-            moveFruits();
-            moveCharater();
-        },30)
 
+    if (event.key === ' ') {
         const jump = setInterval(function () {
             charY -= 10;
             if (charY == 350) {
                 clearInterval(jump);
                 setTimeout(function () {
-                    var land = setInterval(function () {
-                        charY+=10;
+                    let land = setInterval(function () {
+                        charY += 10;
                         if (charY == 430) {
                             clearInterval(land)
-                            clearInterval(moveBG2)
                         }
                     }, 30)
-                }, 400)
+                }, 200)
             }
         }, 30)
-    
+
     }
 
 }
 function keyupHandler(event) {
-    delete keyPressed[event.key];
+    if (event.key === 'ArrowRight') {
+        rightEvent = false;
+    }
 }
 function getScore() {
     const mainScore = document.querySelector('.score');
-
     mainScore.innerText = `Score : ${score}`;
 }
 function draw() {
@@ -131,7 +127,7 @@ function draw() {
                 y: canvas.height - ground.height - tree.height + 50
             })
         }
-        if(trees[i].x === -tree.width){
+        if (trees[i].x === -tree.width) {
             trees.shift();
         }
     }
@@ -146,46 +142,57 @@ function draw() {
                 y: canvas.height - ground.height + 50
             })
         }
-        if(spikes[i].x === -spike1.width){
+        if (spikes[i].x === -spike1.width) {
             spikes.shift();
         }
     }
 
     //add fruits
     for (let i = 0; i < fruits.length; i++) {
+
         context.drawImage(fruit, fruits[i].x, fruits[i].y);
         if (fruits[i].x === 400) {
             // const randomF = Math.floor(Math.random()*5)+1;
+            // const fruitSrc = `images/fruit${randomF}.png`;
+            
             fruits.push({
                 x: canvas.width,
                 y: canvas.height - ground.height - 70
-            }
-            )
+            })
         }
-        if(fruits[i].x === -fruit.width){
+        if (fruits[i].x === -fruit.width) {
             fruits.shift();
+        }
+        //eat fruits
+        if (charX + manr1.width >= fruits[i].x && charX<=fruits[i].x+fruit.width
+            &&charY<=fruits[i].y+fruit.height&&charY+manr1.height>=fruits[i].y) {
+            fruits.splice(i,1);
+            eat.play();
+        }else if(charX + manr2.width >= fruits[i].x && charX<=fruits[i].x+fruit.width
+            &&charY<=fruits[i].y+fruit.height&&charY+manr2.height>=fruits[i].y){
+            fruits.splice(i,1);
+            eat.play();
         }
     }
 
     //character walk Image
     if (charM) {
-        context.drawImage(manr1, 80, charY, 80, 80);
+        context.drawImage(manr1, charX, charY, 80, 80);
     } else {
-        context.drawImage(manr2, 80, charY, 80, 80);
+        context.drawImage(manr2, charX, charY, 80, 80);
     }
-
-    //eat fruits
-    
+    if (rightEvent) {
+        movemove();
+    }
     //gameover
 
-    
     getScore();
     requestAnimationFrame(draw)
 }
 function init() {
+    gamestart.play();
     draw()
     document.addEventListener('keydown', keydownHandler)
     document.addEventListener('keyup', keyupHandler)
-    // document.addEventListener('keypress',keypressHandler)
 }
 init();
